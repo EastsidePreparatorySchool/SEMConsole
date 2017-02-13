aeb39584675466600b35c31141275f7051309eb8
Gunnar Mein <gmein@eastsideprep.org>
2017-02-13 07:50:13 -0800

    FS + files
    Fullscreen changes, not done, and file saving, mostly done.

--- Console/src/console/Console.java
+++ Console/src/console/Console.java
@@ -5,21 +5,26 @@
  */
 package console;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.concurrent.LinkedTransferQueue;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -28,7 +33,7 @@ import javafx.scene.text.Text;
 public class Console extends Application {
 
     private static SEMThread semThread;
-    private BorderPane root;
+    private Pane root;
     private HBox top;
     private ImageView view;
     private LinkedTransferQueue<SEMImage> ltq;
@@ -39,6 +44,8 @@ public class Console extends Application {
     private Text txt;
     private Scene scene;
     private Stage stage;
+    private BorderPane bp;
+    private boolean fullScreen = false;
 
     static private ConsolePane cp;
     static private boolean printOff = false;
@@ -51,7 +58,8 @@ public class Console extends Application {
         this.stage = primaryStage;
 
         primaryStage.setTitle("SEM Console");
-        this.root = new BorderPane();
+        this.bp = new BorderPane();
+        this.root = this.bp;
 
         // top line - controls
         top = new HBox();
@@ -62,43 +70,34 @@ public class Console extends Application {
         btn.setOnAction((event) -> startSEMThread());
 
         Button btn2 = new Button("Full screen");
-        btn2.setOnAction((event) -> {
-            if (stage.isFullScreen()) {
-                btn2.setText("Full screen");
-                primaryStage.setFullScreen(false);
-            } else {
-                btn2.setText("Exit full screen");
-                primaryStage.setFullScreenExitHint("Press any key to exit full screen mode");
-                primaryStage.setFullScreenExitKeyCombination(null);
-                primaryStage.setFullScreen(true);
-            }
+        btn2.setOnAction((ActionEvent event) -> {
+        
+            primaryStage.setFullScreenExitHint("Press any key to exit full screen mode");
+            primaryStage.setFullScreenExitKeyCombination(null);
+            primaryStage.setFullScreen(true);
+ 
+
         }
         );
-        
-        top.setOnMouseMoved((e) -> {top.setOpacity(1.0);});
+        Button btn3 = new Button("Save as ...");
+        btn3.setOnAction((event) -> saveFile());
 
         txt = new Text("Not connected");
         HBox h = new HBox();
 
-        h.getChildren()
-                .add(txt);
-        h.setPadding(
-                new Insets(6, 12, 6, 12));
+        h.getChildren().add(txt);
+        h.setPadding(new Insets(6, 12, 6, 12));
 
-        top.setPadding(
-                new Insets(15, 12, 15, 12));
-        top.getChildren()
-                .addAll(btn, h, btn2);
-        root.setTop(top);
+        top.setPadding(new Insets(15, 12, 15, 12));
+        top.getChildren().addAll(btn, h, btn2, btn3);
+        bp.setTop(top);
         cp = new ConsolePane();
 
-        cp.setPrefWidth(
-                960);
+        cp.setPrefWidth(960);
         BorderPane.setAlignment(cp, Pos.CENTER);
 
-        BorderPane.setMargin(cp,
-                new Insets(10, 8, 10, 8));
-        root.setBottom(cp);
+        BorderPane.setMargin(cp, new Insets(10, 8, 10, 8));
+        bp.setBottom(cp);
 
         // placeholder for img
         StackPane sp = new StackPane();
@@ -110,9 +109,9 @@ public class Console extends Application {
         sp.setMinHeight(540);
         sp.setPrefHeight(540);
 
-        root.setCenter(sp);
+        bp.setCenter(sp);
 
-        this.scene = new Scene(root);
+        this.scene = new Scene(bp);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -120,7 +119,7 @@ public class Console extends Application {
     private void displayImage(WritableImage img) {
         // if old view is here, remove from display
         if (this.view != null) {
-            this.root.setCenter(null);
+            this.bp.setCenter(null);
             this.view = null;
         }
 
@@ -142,7 +141,7 @@ public class Console extends Application {
         });
         this.view.fitWidthProperty().bind(this.stage.widthProperty());
 
-        this.root.setCenter(this.view);
+        this.bp.setCenter(this.view);
 
     }
 
@@ -236,6 +235,25 @@ public class Console extends Application {
         return result;
     }
 
+    public void saveFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Image");
+        System.out.println(view.getId());
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            try {
+                if (!file.getName().toLowerCase().endsWith("png")) {
+                    // TODO: create new file object with added extension
+                }
+                ImageIO.write(SwingFXUtils.fromFXImage(view.getImage(), null), "png", file);
+                Console.println("Image written to " + file.getName());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    // console util functions
     public static void println(String s) {
         if (!printOff) {
             cp.println(s);
@@ -262,6 +280,7 @@ public class Console extends Application {
         printOff = false;
     }
 
+    // main fx launcher
     public static void main(String[] args) {
         try {
             launch(args);
